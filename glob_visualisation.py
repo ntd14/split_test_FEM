ac6420a664807fb2f91e960f2606ce3c14475b1b
ntd14 <ntd14@uclive.ac.nz>
2015-10-28 11:54:33 +1300

    have all constants etc ready to go

--- glob_visualisation.py
+++ glob_visualisation.py
@@ -5,19 +5,25 @@ import sys as sys
 import pandas as pandas
 from dolfin.cpp._mesh import Cell_get_cell_data, Cell_get_vertex_coordinates,\
     Cell_normal, Cell_cell_normal, Cell_contains
+from cmath import pi
 
 mesh = Mesh("./final_xml/glob1.xml")
 
 df = pandas.read_csv('glob1_properties.csv', sep=',')
 
-print(df)
-print(df['log_ID'])
+BED = float(df['BED'][0])
+SED = float(df['SED'][0])
+slit_length = float(df['slit_length'][0])
+opening = float(df['opening'][0])
+log_len = float(df['log_len'][0])
+split_strain = opening*BED/(1.74*slit_length**2)/1000000
+El = 10000000000.0
+split_stress = El*split_strain
+df['rad_pos'] = (df['rad_pos']-1)*(pi/2)
+df['cirad_strain'] = 11.6*df['cirad']/1000
+df['cirad_stress'] = El*df['cirad_strain']
 
-BED = df['BED'][0]
-SED = df['SED'][0]
-slit_length = df['slit_length'][0]
-opening = df['opening'][0]
-log_len = df['log_len'][0]
+#function to convert cylindrical to cart and visaversa
 
 # need to some how set the stress/strain at each point we have, 
 # then interpolate that over the whole mesh? setting a netural 
